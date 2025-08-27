1bb4ef1cd0e61186170c30687b4a231652efddcc
eliranovadia7@gmail.com <eliranovadia7@gmail.com>
2025-08-27 17:21:23 +0300

    new weights 27.8

--- model_loader.py
+++ model_loader.py
@@ -1,6 +1,6 @@
 from ultralytics import YOLO
 
 def load_model():
-    model = YOLO("best.pt")
+    model = YOLO("best_27.8.pt")
     print("✅ YOLO model loaded with classes:", model.names)
     return model
